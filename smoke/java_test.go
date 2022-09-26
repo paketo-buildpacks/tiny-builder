dc2e089bc2addeee9654cf1cab31ac33af126b2f
Forest Eckhardt <forest.eckhardt@gmail.com>
2022-09-26 14:20:30 +0000

    Updates name expectation

--- smoke/java_test.go
+++ smoke/java_test.go
@@ -72,7 +72,7 @@ func testJava(t *testing.T, context spec.G, it spec.S) {
 
 			Expect(logs).To(ContainLines(ContainSubstring("Paketo Buildpack for BellSoft Liberica")))
 			Expect(logs).To(ContainLines(ContainSubstring("Paketo Buildpack for Maven")))
-			Expect(logs).To(ContainLines(ContainSubstring("Paketo Executable JAR Buildpack")))
+			Expect(logs).To(ContainLines(ContainSubstring("Paketo Buildpack for Executable JAR")))
 			Expect(logs).To(ContainLines(ContainSubstring("Paketo Buildpack for Spring Boot")))
 		})
 	})
